eedd6c12d0e7f7f0cf22e45b6875fa793ad52bbd
Eltereon <true.false1805@gmail.com>
2021-08-05 22:45:42 +0400

    Update q-learning program.py

--- q-learning/q-learning program.py
+++ q-learning/q-learning program.py
@@ -9,21 +9,16 @@ import numpy as np
 import random as random
 
 environment_states = 2 # Состояния
-q_values = np.zeros((environment_states, 2)) # матрица значений Q
+q_values = np.zeros((environment_states, 2)) # матрица значений Q изначально нулевая
 
-probabilities = [0.7,0.3, 0.5,0.5, 0.6,0.4, 0.8,0.2]
-rewards = [10,6, 5,4, 4,-2, 1,-5]
+probabilities = [0.3,0.7, 0.7,0.3, 0.5,0.5, 0.5,0.5]
+rewards = [-10,15,-10,5,5,-10,2,-10]
 
-actions = ['strategy_1', 'strategy_2']
-
-def is_finish(cycle_step):
-    if cycle_step == 1000.:
-        return True
-    else:
-        return False
+actions = ['strategy_1', 'strategy_2'] 
     
 def get_starting_state():
-    current_state_index = np.random.randint(environment_states)
+    starting_state_index = np.random.randint(environment_states)
+    return starting_state_index
 
 def get_next_action(current_state_index, epsilon):
     if np.random.random() < epsilon:
@@ -31,16 +26,73 @@ def get_next_action(current_state_index, epsilon):
     else: #выбирается случайное действие
         return np.random.randint(2)
 
+
 def get_next_state(current_state_index, action_index):
-  new_state_index = current_state_index
   temp_probability = np.random.random()
-  if current_state_index == 1 and actions[action_index] == 'strategy_1' and np.random.random() > probabilities[0]:
-    new_state_index == 1
+  new_state_index = 0 
+  reward = 0
+  if current_state_index == 0 and actions[action_index] == 'strategy_1' and temp_probability < probabilities[0]:
+    new_state_index = 0
+    reward = rewards[0]
+  elif current_state_index == 0 and actions[action_index] == 'strategy_1' and temp_probability > probabilities[0]:
+    new_state_index = 1
+    reward = rewards[1]
+  elif current_state_index == 0 and actions[action_index] == 'strategy_2' and temp_probability < probabilities[2]:
+    new_state_index = 0
+    reward = rewards[2]
+  elif current_state_index == 0 and actions[action_index] == 'strategy_2' and temp_probability > probabilities[2]:
+    new_state_index = 1
+    reward = rewards[3]
+  if current_state_index == 1 and actions[action_index] == 'strategy_1' and temp_probability < probabilities[4]:
+    new_state_index = 0
+    reward = rewards[4]
+  elif current_state_index == 1 and actions[action_index] == 'strategy_1' and temp_probability > probabilities[4]:
+    new_state_index = 1
+    reward = rewards[5]
+  elif current_state_index == 1 and actions[action_index] == 'strategy_2' and temp_probability < probabilities[6]:
+    new_state_index = 0
+    reward = rewards[6]
+  elif current_state_index == 1 and actions[action_index] == 'strategy_2' and temp_probability > probabilities[6]:
+    new_state_index = 1 
+    reward = rewards[7]
+  return new_state_index, reward
 
-  elif current_state_index == 1 and actions[action_index] == 'strategy_2':
-    new_row_index += 1
+epsilon = 0.8
+discount_factor = 0.5
+learning_rate = 0.1
+cycle_step = 0
 
-  return new_state_index
 
-# Заметка ? реализовать случ.переход по вероятностям через веса random.choises или как задумал ?
+for episode in range(100000):
+  state_index = get_starting_state()
+  while cycle_step < 1000: 
+    action_index = get_next_action(state_index, epsilon) 
+    old_state_index = state_index 
+    state_index, reward = get_next_state(state_index, action_index) 
+    old_q_value = q_values[old_state_index, action_index]
+    temporal_difference = reward + (discount_factor * np.max(q_values[state_index])) - old_q_value   
+    new_q_value = old_q_value + (learning_rate * temporal_difference)
+    q_values[old_state_index, action_index] = new_q_value
+    cycle_step += 1
+
+for i in range(4):
+    print()
+
+print("Q-matrix:")
+
+for i in range(2):
+    for j in range(2):
+        print(q_values[i][j], end = ' ')
+    print()
+
+for i in range(2):
+    print()
+    
+print("Best solutions")
+print()
+print('In state 1:','', np.argmax(q_values[0])+1)
+print('In state 2:','', np.argmax(q_values[1])+1 )
+
+    
+# Заметка: для рандома можно использовать веса
 
