a5ffa7b6ecef5aa576a02d060b84d402d2a644ba
Eltereon <true.false1805@gmail.com>
2021-08-10 21:54:51 +0400

    Update Program.cs

--- Iterative/Program.cs
+++ Iterative/Program.cs
@@ -1,24 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Iterative
+namespace Iterative_method_for_sequential_decision_processes
 {
     class Program
+
     {
+      
+        static List<double> GaussianEllimination(double[,] coefficients, int countEq)
+        {
+            int row = countEq;
+            double d, s;
+
+            double[,] A = new double[row, row];
+            double[,] a = new double[row, row];
+
+            double[] B = new double[row];
+            double[] b = new double[row];
+            double[] x = new double[row];
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < row; j++)
+                {
+                    A[i, j] = coefficients[i, j];
+                    a[i, j] = A[i, j];
+                }
+                B[i] = coefficients[i, countEq]; // Последний элемент в коэффициентах - свободный член
+                b[i] = B[i];
+            }
+
+            for (int k = 0; k < row; k++)
+            {
+                for (int j = k + 1; j < row; j++)
+                {
+                    d = A[j, k] / A[k, k];
+                    for (int i = k; i < row; i++)
+                    {
+                        A[j, i] = A[j, i] - d * A[k, i];
+                    }
+                    B[j] = B[j] - d * B[k];
+                }
+            }
+
+            for (int k = row - 1; k >= 0; k--)
+            {
+                d = 0;
+                for (int j = k; j < row; j++)
+                {
+                    s = A[k, j] * x[j];
+                    d += s;
+                }
+                x[k] = (B[k] - d) / A[k, k];
+            }
+
+            List<double> Answer = new List<double>();
+            for (int i = 0; i < row; i++)
+            {
+                Answer.Add(x[i]);
+            }
+
+            return Answer;
+        }
+
         class State
         {
-            private List<Strategy> Strategies = new List<Strategy>(); // Список стратегий состояния
+            private List<Strategy> Strategies = new List<Strategy>();
 
-            public State() // Пустой конструктор
+            public State()
             {
             }
 
-            public List<Strategy> GetStrategies() // Извлечение стратегий
+            public List<Strategy> GetStrategies()
             {
                 return Strategies;
             }
 
-            public void AddStrategy(Strategy s) // Добавление стратегии
+            public void AddStrategy(Strategy s)
             {
                 Strategies.Add(s);
             }
@@ -26,22 +84,22 @@ namespace Iterative
 
         class Strategy
         {
-            private List<double> P = new List<double>(); // Вероятность перехода для данной стратегии
-            private double q; // Ожидаемый базовый доход
+            private List<double> P = new List<double>();
+            private double q;
 
-            public Strategy(string[] str) // Конструктор стратегии
+            public Strategy(string[] str)
             {
                 q = Convert.ToDouble(str[str.Length - 1]);
                 for (int sn = 0; sn < str.Length - 1; sn++)
                     P.Add(Convert.ToDouble(str[sn]));
             }
 
-            public double GetQ() // Извлечение ожидаемого базового дохода
+            public double GetQ() 
             {
                 return q;
             }
 
-            public List<double> GetP() // Извлечение вероятностей перехода
+            public List<double> GetP()
             {
                 return P;
             }
@@ -59,12 +117,12 @@ namespace Iterative
             List<State> States = new List<State>();
             for (int i = 0; i < nstates; i++)
             {
-                Console.WriteLine("Enter number of strategies for " + i + " state:");
+                Console.WriteLine("Enter number of strategies for " + (i + 1) + " state:");
                 int nstrategies = Convert.ToInt32(Console.ReadLine());
                 State bufstate = new State();
                 for (int j = 0; j < nstrategies; j++)
                 {
-                    Console.WriteLine("Enter a " + j + " strategy:");
+                    Console.WriteLine("Enter a " + (j + 1) + " strategy:");
                     string input = Console.ReadLine();
                     string[] words = input.Split(' ');
                     Strategy bufstrategy = new Strategy(words);
@@ -73,7 +131,7 @@ namespace Iterative
                 States.Add(bufstate);
             }
 
-            // Вычисление начального положения
+ 
             List<int> d = new List<int>();
             List<Strategy> pq = new List<Strategy>();
             for (int i = 0; i < nstates; i++)
@@ -92,27 +150,49 @@ namespace Iterative
                 pq.Add(States[i].GetStrategies()[nbest]);
             }
 
-
-            // Улучшение решения
-            for (int i = 0; i < nstates; i++)
+            List<int> lastd = new List<int>();
+            while (d.Equals(lastd))
             {
-                double maxQ = Double.NegativeInfinity;
-                for (int j = 0; j < States[i].GetStrategies().Count; j++)
+                // g + v1 = q1 + p11 * v1 + p12 * v2 |-----\ | g + (1 - p11) * v1 = q1
+                // g + v2 = q2 + p21 * v1 + p22 * v2 |-----/ | g + (-p21) * v1 = q2
+                double[,] coefficients = new double[2, 3];
+                coefficients[0, 0] = 1;
+                coefficients[0, 1] = 1 - pq[0].GetP()[0];
+                coefficients[0, 2] = pq[0].GetP()[0];
+                coefficients[1, 0] = 1;
+                coefficients[1, 1] = -pq[0].GetP()[0];
+                coefficients[1, 2] = pq[0].GetP()[0];
+                List<double> gausAns = GaussianEllimination(coefficients, coefficients.GetLength(0));
+                double g = gausAns[0];
+                double v1 = gausAns[1];
+                double v2 = 0;
+
+                lastd.Clear();
+                for (int i = 0; i < d.Count; i++)
+                    lastd.Add(d[i]);
+
+                // Улучшение решения
+                for (int i = 0; i < nstates; i++)
                 {
-                    double param = States[i].GetStrategies()[j].GetQ() + States[i].GetStrategies()[j].GetP()[0] * v1;
-                    if (param > maxQ)
+                    double maxQ = Double.NegativeInfinity;
+                    for (int j = 0; j < States[i].GetStrategies().Count; j++)
                     {
-                        d[i] = j;
-                        maxQ = param;
+                        double param = States[i].GetStrategies()[j].GetQ() + States[i].GetStrategies()[j].GetP()[0] * v1;
+                        if (param > maxQ)
+                        {
+                            d[i] = j;
+                            maxQ = param;
+                        }
                     }
                 }
+
+                for (int i = 0; i < States.Count; i++)
+                    pq[i] = States[i].GetStrategies()[d[i]];
             }
 
-            for (int i = 0; i < States.Count; i++)
-                pq[i] = States[i].GetStrategies()[d[i]];
-
-
+            Console.WriteLine("Best solution:");
+            for (int i = 0; i < d.Count; i++)
+                Console.Write((d[i] + 1) + " ");
         }
-    }
     }
 }
