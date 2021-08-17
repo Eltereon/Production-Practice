4ec021b6a12644691c05cea235707f3cd5188584
Eltereon <true.false1805@gmail.com>
2021-08-18 01:38:19 +0400

    Update network q-learning.py

--- q-learning networks/network q-learning.py
+++ q-learning networks/network q-learning.py
@@ -1,35 +1,24 @@
-!pip install -q tf-agents
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+import base64
+import imageio
+import IPython
 import matplotlib.pyplot as plt
-
-import abc
-import tensorflow as tf
 import numpy as np
+import random
 
+import tensorflow as tf
+from tf_agents.agents.reinforce import reinforce_agent
+from tf_agents.environments import suite_gym
 from tf_agents.environments import py_environment
 from tf_agents.environments import tf_environment
 from tf_agents.environments import tf_py_environment
-from tf_agents.environments import utils
 from tf_agents.specs import array_spec
-from tf_agents.environments import wrappers
-from tf_agents.environments import suite_gym
+from tf_agents.networks import actor_distribution_network
+from tf_agents.replay_buffers import tf_uniform_replay_buffer
+from tf_agents.trajectories import trajectory
+from tf_agents.utils import common
 from tf_agents.trajectories import time_step as ts
 
-tf.compat.v1.reset_default_graph
-inputs1 = tf.compat.v1.placeholder(shape=[1,2],dtype=tf.float32)
-W = tf.Variable(tf.compat.v1.random_uniform([2,2]))
-Qout = tf.matmul(inputs1,W)
-predict = tf.argmax(Qout,1)
-
-nextQ = tf.compat.v1.placeholder(shape=[1,2],dtype=tf.float32)
-loss = tf.reduce_sum(tf.square(nextQ - Qout))
-trainer = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=0.1)
-updateModel = trainer.minimize(loss)
-
-init = tf.compat.v1.initialize_all_variables()
+rewards = []
 
 class MyEnvironment(py_environment.PyEnvironment):
 
@@ -40,6 +29,7 @@ class MyEnvironment(py_environment.PyEnvironment):
         shape=(1,), dtype=np.int32, minimum=0, name='observation')
     self._state = 0
     self._episode_ended = False
+    self._count_steps = 0
 
   def action_spec(self):
     return self._action_spec
@@ -53,67 +43,148 @@ class MyEnvironment(py_environment.PyEnvironment):
     return ts.restart(np.array([self._state], dtype=np.int32))
   
   def _step(self, action):
-    reward = 0   
-    testArray = np.array([
+
+    self._count_steps += 1
+
+    if self._episode_ended:
+      # The last action ended the episode. Ignore the current action and start
+      # a new episode.
+      return self.reset()
+
+    if self._count_steps == 30:
+      self._episode_ended = True
+    else:
+
+      testArray = np.array([
                       [[0.3,0.7], [0.7,0.3], [[-10,15], [-10,5]]],
                       [[0.5,0.5], [0.5,0.5], [[5,-10], [2,-10]]]
                     ]) 
-    weights = testArray[self._state][action]
-    current_state = self._state
-    length = len(testArray[self._state]) - 1
-    self._state = random.choices([0,1], weights)[0]
-    reward = testArray[current_state][length][action][self._state]
+      weights = testArray[self._state][action]
+      current_state = self._state
+      length = len(testArray[self._state]) - 1
+      self._state = random.choices([0,1], weights)[0]
+      rewards.append(testArray[current_state][length][action][self._state])
+    if self._episode_ended:
+      reward = np.sum(rewards)
+      list.clear(rewards)
+      return ts.termination(np.array([self._state], dtype=np.int32), reward)
+    else:
+      return ts.transition(
+          np.array([self._state], dtype=np.int32), reward=0.0, discount=1.0)
+      
+tf.compat.v1.enable_v2_behavior()
 
-    return ts.termination(np.array([self._state], dtype=np.int32), reward)
-         
-environment = MyEnvironment()
-#action1 = 0
-#action2 = 1
-#time_step = environment.reset()
-#print(time_step)
-#cumulative_reward = time_step.reward
-#cycle_step = 0
-#for _ in range(29):
-#  time_step = environment.step(0)
-#  print(time_step.observation)
-#  cumulative_reward += time_step.reward
-#print('Final Reward = ', cumulative_reward)
+num_iterations = 250 
+collect_episodes_per_iteration = 2 
+replay_buffer_capacity = 2000
 
-discount_factor = .99
-num_episodes = 1000
-total_rewards = []
+fc_layer_params = (100,)
 
-def Go(epsilon):
-    e = epsilon
-    with tf.compat.v1.Session() as sess:
-        sess.run(init)
-        for i in range(num_episodes):
-            time_step = environment.reset()
-            reward_sum = 0
-            j = 0
-            while j < 30:
-                j+=1
-                s = time_step.observation[0]
-                a,allQ = sess.run([predict,Qout],feed_dict={inputs1:np.identity(2)[s:s+1]})
-                if np.random.rand(1) > e:
-                    a[0] = np.random.randint(2)
-                time_step = environment.step(a[0])
-                reward = time_step.reward
-                s1 = time_step.observation[0]
-                Q1 = sess.run(Qout,feed_dict={inputs1:np.identity(2)[s1:s1+1]})
-                maxQ1 = np.max(Q1)
-                targetQ = allQ
-                targetQ[0,a[0]] = reward + discount_factor*maxQ1
-                W1 = sess.run([updateModel,W],feed_dict={inputs1:np.identity(2)[s:s+1],nextQ:targetQ})
-                reward_sum += time_step.reward
-                
-            total_rewards.append(reward_sum)
+learning_rate = 1e-3 
+log_interval = 25 
+num_eval_episodes = 10 
+eval_interval = 50 
 
-Go(0.9)
-plt.plot(total_rewards)
-plt.show
-print(np.mean(total_rewards))
-Go(0)
-plt.plot(total_rewards)
-plt.show
-print(np.mean(total_rewards))
+env = MyEnvironment()
+
+train_py_env = MyEnvironment()
+eval_py_env = MyEnvironment()
+
+train_env = tf_py_environment.TFPyEnvironment(train_py_env)
+eval_env = tf_py_environment.TFPyEnvironment(eval_py_env)
+
+actor_net = actor_distribution_network.ActorDistributionNetwork(
+    train_env.observation_spec(),
+    train_env.action_spec(),
+    fc_layer_params=fc_layer_params)
+
+optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+
+train_step_counter = tf.compat.v2.Variable(0)
+
+tf_agent = reinforce_agent.ReinforceAgent(
+    train_env.time_step_spec(),
+    train_env.action_spec(),
+    actor_network=actor_net,
+    optimizer=optimizer,
+    normalize_returns=True,
+    train_step_counter=train_step_counter)
+tf_agent.initialize()
+
+eval_policy = tf_agent.policy
+collect_policy = tf_agent.collect_policy
+
+def compute_avg_return(environment, policy, num_episodes=1):
+
+  total_return = 0.0
+  for _ in range(num_episodes):
+
+    time_step = environment.reset()
+    episode_return = 0.0
+
+    while not time_step.is_last():
+      action_step = policy.action(time_step)
+      time_step = environment.step(action_step.action)
+      episode_return += time_step.reward
+    total_return += episode_return
+
+  avg_return = total_return / num_episodes
+  return avg_return.numpy()[0]
+
+replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
+    data_spec=tf_agent.collect_data_spec,
+    batch_size=train_env.batch_size,
+    max_length=replay_buffer_capacity)
+
+def collect_episode(environment, policy, num_episodes):
+
+  episode_counter = 0
+  environment.reset()
+
+  while episode_counter < num_episodes:
+    time_step = environment.current_time_step()
+    action_step = policy.action(time_step)
+    next_time_step = environment.step(action_step.action)
+    traj = trajectory.from_transition(time_step, action_step, next_time_step)
+
+    # Add trajectory to the replay buffer
+    replay_buffer.add_batch(traj)
+
+    if traj.is_boundary():
+      episode_counter += 1
+
+try:
+  %%time
+except:
+  pass
+
+# (Optional) Optimize by wrapping some of the code in a graph using TF function.
+tf_agent.train = common.function(tf_agent.train)
+
+# Reset the train step
+tf_agent.train_step_counter.assign(0)
+
+# Evaluate the agent's policy once before training.
+avg_return = compute_avg_return(eval_env, tf_agent.policy, num_eval_episodes)
+returns = [avg_return]
+
+for _ in range(num_iterations):
+
+  # Collect a few episodes using collect_policy and save to the replay buffer.
+  collect_episode(
+      train_env, tf_agent.collect_policy, collect_episodes_per_iteration)
+
+  # Use data from the buffer and update the agent's network.
+  experience = replay_buffer.gather_all()
+  train_loss = tf_agent.train(experience)
+  replay_buffer.clear()
+
+  step = tf_agent.train_step_counter.numpy()
+
+  if step % log_interval == 0:
+    print('step = {0}: loss = {1}'.format(step, train_loss.loss))
+
+  if step % eval_interval == 0:
+    avg_return = compute_avg_return(eval_env, tf_agent.policy, num_eval_episodes)
+    print('step = {0}: Average Return = {1}'.format(step, avg_return))
+    returns.append(avg_return)
